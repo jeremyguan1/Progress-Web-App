1627750edc7ce57db6d613beb3922f2720778847
Jeremy Guan <jeremyguan1@gmail.com>
2019-11-06 17:25:27 -0800

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,18 @@
-
-self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
+self.addEventListener("install", function(event) {
+  console.log("[Service Worker] Installing Service Worker ...", event);
+  event.waitUntil(
+    caches.open("static").then(caches => {
+      console.log("[Service Worker] Precaching App Shell");
+      caches.add("/src/js/app.js");
+    })
+  );
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ....', event);
+self.addEventListener("activate", function(event) {
+  console.log("[Service Worker] Activating Service Worker ....", event);
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function(event) {
   event.respondWith(fetch(event.request));
 });
